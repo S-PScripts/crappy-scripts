7a1fb0e4641e091fecd8bddedaf18fd78a66b273
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-01 21:28:55 +0000

    Create VG Crash.lua

---
+++ VG Crash.lua
@@ -0,0 +1,14 @@
+game.Players:Chat(":gear me 000000000000000000000000000000000000094794847")
+local Backpack = game.Players.LocalPlayer:FindFirstChildOfClass("Backpack")
+game.Players.LocalPlayer.Backpack:WaitForChild("VampireVanquisher")
+for _, v in ipairs(Backpack:GetChildren()) do
+   v.Parent = game.Players.LocalPlayer.Character
+   v:Activate()
+end
+wait(.15)
+task.spawn(function()
+    while true do
+       game.Players:Chat("unsize me me me")
+       task.wait()
+    end
+end)
