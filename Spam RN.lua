1b5cd66afe5052b72603150e336ad621714fe0ff
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-01 21:54:55 +0000

    Create Spam RN.lua

---
+++ Spam RN.lua
@@ -0,0 +1,18 @@
+local function spammer()
+    while spam == true do
+	         Chat(spammer)
+           time.wait(0)
+    end
+end
+
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+     local command = string.lower(msg)
+      if string.sub(msg:lower(), 0, 5) == ".spam" then 
+           spammer = string.sub(msg:lower(), 7)
+           spam = true
+           spammer()
+      end
+      if command == ".unspam" then
+           spam = false
+      end
+end)
