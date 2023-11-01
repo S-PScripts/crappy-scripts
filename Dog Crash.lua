378f4dc512728996e84ca8e479734f0d2d2191c2
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-01 21:29:56 +0000

    Create Dog Crash.lua

---
+++ Dog Crash.lua
@@ -0,0 +1,8 @@
+--ONLY WORKS IN KAH NBC
+task.spawn(function()
+    while true do
+        game.Players:Chat("dog all all all")
+        game.Players:Chat("clone all all all")
+        task.wait()
+    end
+end)
