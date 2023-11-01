6429da069bde0f2be85e42fe2e59bc8ab16c2e98
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-01 21:39:57 +0000

    Create Slow Player.lua

---
+++ Slow Player.lua
@@ -0,0 +1,18 @@
+function slowuser()
+        while slowuser == true do
+              game.Players:Chat("gear ".. caruser .." 253519495")
+              time.wait(0)
+        end
+end
+
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+    local command = string.lower(msg)
+    if string.sub(msg:lower(), 0, 8) == ".carspam" then
+       slowuser = true
+       caruser = string.sub(msg:lower(), 10)
+       slowuser()
+    end
+    if command == ".uncarspam" then
+        slowuser = false
+    end
+end)
