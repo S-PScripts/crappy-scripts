62e89cf0ac3aceebebd15ab07401a09f9134321d
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-01 21:43:50 +0000

    Create Anti Crash.lua

---
+++ Anti Crash.lua
@@ -0,0 +1,50 @@
+local anticrash = true
+
+local blacklistedTools = {"OrinthianSwordAndShield", "VampireVanquisher"}
+
+local function warnCrash(player, toolName)
+       Chat("ungear " .. player.Name)
+       Chat("punish " .. player.Name)
+       Chat("h \n\n\n Sorry, " .. player.Name .. ", you cannot use " .. toolName .. " because of anticrash. \n\n\n")
+       Chat("clr")
+end
+
+local function checkPlayerBackpack(player)
+    local backpack = player:FindFirstChild("Backpack")
+    if backpack then
+        for _, toolName in ipairs(blacklistedTools) do
+            local tool = backpack:FindFirstChild(toolName)
+            if tool and anticrash then
+		if player.Name == game.Players.LocalPlayer.Name then
+                       break
+                else
+                       warnCrash(player, toolName)
+                       break
+                end
+            end
+        end
+    end
+end
+
+-- backpack gear
+game.Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(character)
+        checkPlayerBackpack(player)
+    end)
+    checkPlayerBackpack(player)
+end)
+
+-- backpack gear
+game:GetService("RunService").Heartbeat:Connect(function()
+    for _, player in ipairs(game.Players:GetPlayers()) do
+        if player.Character then
+            checkPlayerBackpack(player)
+        end
+    end
+end)
+
+for _, player in ipairs(game.Players:GetPlayers()) do
+    if player.Character then
+        checkPlayerBackpack(player)
+    end
+end
