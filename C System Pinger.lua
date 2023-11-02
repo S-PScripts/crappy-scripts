91c8f3ae87fb349fbdef89b54ddf5d3ca53109e4
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-02 18:14:55 +0000

    esex

---
+++ C System Pinger.lua
@@ -0,0 +1,15 @@
+local csystem = true
+local connections = {}
+
+plr.Chatted:Connect(function(msg)
+    if csystem then 
+        for _, v in pairs(game.Players:GetPlayers()) do
+            local connection = v.Chatted:Connect(function(message)
+                if message:lower():find("/w") or message:lower():find("/c") and v ~= game.Players.LocalPlayer then
+                    print(v.Name..' is using /c system or whispering commands.')
+                end
+            end)
+            table.insert(connections, connection)
+        end
+    end
+end)
