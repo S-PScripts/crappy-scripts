5f635e0f9258be9cad5c8ef9aca96241b2b9b423
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-02 18:27:18 +0000

    Create noob detector.lua

---
+++ noob detector.lua
@@ -0,0 +1,15 @@
+local noobs = true
+local connections = {}
+
+plr.Chatted:Connect(function(msg)
+    if noobs then 
+        for _, v in pairs(game.Players:GetPlayers()) do
+            local connection = v.Chatted:Connect(function(message)
+                if message:lower() == ";fly" or message:lower() == ";fly me" then
+                   print(v.Name..' is a noob.')
+                   game:Players:Chat(v.Name..', it is fly me, not ;fly!!")
+            end)
+            table.insert(connections, connection)
+        end
+    end
+end)
