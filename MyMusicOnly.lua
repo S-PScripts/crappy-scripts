be8b4245b5c91862a11d5f1a79d2b3a8471d8a4a
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-02 17:48:25 +0000

    Create MyMusicOnly.lua

---
+++ MyMusicOnly.lua
@@ -0,0 +1,38 @@
+local function musiclover()
+  while true do
+    if mymusiconly == true and musicoff == false then
+		  if game:GetService("Workspace").Terrain["_Game"].Folder:FindFirstChild("Sound") then
+				if game:GetService("Workspace").Terrain["_Game"].Folder.Sound.SoundId == "http://www.roblox.com/asset/?id="..mymusiconlyid then
+                    game.Players:Chat("")
+    			else
+    				game.Players:Chat("music "..mymusiconlyid)
+                end
+          end
+          if not game:GetService("Workspace").Terrain["_Game"].Folder:FindFirstChild("Sound") then
+				game.Players:Chat("music "..mymusiconlyid)
+          end
+    time.wait(0)
+   end
+  end
+end
+
+musicoff = true
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+    local command = string.lower(msg)
+    if  string.sub(msg:lower(), 0, 10) == ".permmusic" then
+        musicoff = false
+        mymusiconly = true
+        mymusiconlyid = string.sub(msg:lower(), 12)
+        musiclover()
+    end
+    if command == ".unpermmusic" then
+        mymusiconly = false
+        musicoff = true
+    end
+    if command == ".offmusic" then
+        musicoff = true
+    end
+    if command == ".onmusic" then
+        musicoff = false
+    end
+end)
