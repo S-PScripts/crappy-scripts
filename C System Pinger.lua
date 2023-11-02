46e56fe1865e72a7dca968001222158eef6ea2b5
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-02 22:17:07 +0000

    Update C System Pinger.lua

--- C System Pinger.lua
+++ C System Pinger.lua
@@ -7,7 +7,7 @@ plr.Chatted:Connect(function(msg)
             local connection = v.Chatted:Connect(function(message)
                 if message:lower():find("/w") or message:lower():find("/c") and v ~= game.Players.LocalPlayer then
                     print(v.Name..' is using /c system or whispering commands.')
-                    game.Players:Chat(v.Name..' is using /c system or whispering commands.')
+                    game.Players:Chat('h '..v.Name..' is using /c system or whispering commands.')
                 end
             end)
             table.insert(connections, connection)
