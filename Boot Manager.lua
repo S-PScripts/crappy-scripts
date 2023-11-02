936d4c2f145d7825ffd70f9357fc68bd3ebb52a4
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-02 17:23:05 +0000

    Create Boot Manager.lua

---
+++ Boot Manager.lua
@@ -0,0 +1,14 @@
+myversion = "v1.9
+currentversion = "v2.0" --you keep this on a seperate loadstring. you obsfucate the code with myversion. if a user attempts to boot an old obfsucated version it will fail
+
+errorcode = "ERROR: Invalid Version."
+if not myversion == currentversion then
+    error = true
+    print(errorcode,"Version Detected:",myversion)
+    print("Current Version:",currentversion)
+end
+
+if not error == true then
+		game.Players.LocalPlayer:Kick(errorcode,"Use",currentversion,"instead of",myversion"!")
+    -- while true do end
+end
