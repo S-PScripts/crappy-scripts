6d303e30fdc868c3943f865d5a72815beb95580a
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-02 22:04:58 +0000

    Create ReverseCrash.lua

---
+++ ReverseCrash.lua
@@ -0,0 +1,32 @@
+local noobs = true
+local connections = {}
+
+plr.Chatted:Connect(function(msg)
+    if noobs then 
+        for _, v in pairs(game.Players:GetPlayers()) do
+            local connection = v.Chatted:Connect(function(message)
+                if message:lower():find("94794847") and v ~= game.Players.LocalPlayer then
+                   Crash()
+                end
+            end)
+            table.insert(connections, connection)
+        end
+    end
+end)
+
+local function Crash()
+      game.Players:Chat(":gear me 000000000000000000000000000000000000094794847")
+      local Backpack = game.Players.LocalPlayer:FindFirstChildOfClass("Backpack")
+       game.Players.LocalPlayer.Backpack:WaitForChild("VampireVanquisher")
+      for _, v in ipairs(Backpack:GetChildren()) do
+             v.Parent = game.Players.LocalPlayer.Character
+           v:Activate()
+      end
+      wait(.15)
+      task.spawn(function()
+          while true do
+               game.Players:Chat("unsize me me me")
+               task.wait()
+          end
+      end)
+end
