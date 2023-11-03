e06d84806c19b9f40e52b774a1739f4ce3e6c91d
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-03 16:42:28 +0000

    Update Regen.lua

--- Regen.lua
+++ Regen.lua
@@ -4,4 +4,7 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
           if command == ".regen" then
                fireclickdetector(game:GetService("Workspace").Terrain["_Game"].Admin.Regen.ClickDetector, 0)
           end
+          if command == ".house" then
+               game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-31.0896435, 8.22999477, 70.522644, -0.999961913, 4.495271e-08, -0.0087288795, 4.55292621e-08, 1, -6.58523618e-08, 0.0087288795, -6.62472743e-08, -0.999961913)
+          end
 end)
