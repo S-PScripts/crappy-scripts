2142a2292f22fc2b7538007e4dd1c508e8300615
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-03 09:08:42 +0000

    Update Perm.lua

--- Perm.lua
+++ Perm.lua
@@ -12,28 +12,6 @@ end)
 task.spawn(function()
     while true do 
         task.wait()
-        coroutine.wrap(function() -- PadAbuse
-			      if padAbuse == true then
-				local pads = game:GetService("Workspace").Terrain["_Game"].Admin.Pads:GetChildren("Head")
-			      	for i, pad in pairs(pads) do
-					        coroutine.wrap(function()
-						          pcall(function()
-							        local cre = pad.Head
-						              	local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
-						              	firetouchinterest(cre, spr, 1)
-                            					firetouchinterest(cre, spr, 0)
-                          					firetouchinterest(cre, spr, 1)
-							        task.wait()
-								firetouchinterest(cre, spr, 0)
-							        if pad.Name ~= game.Players.LocalPlayer.Name.."'s admin" then
-							                	fireclickdetector(adminFlr.Regen.ClickDetector, 0)
-							        end
-						          end)
-					        end)()
-				      end
-			    end
-        end)()
-		
         coroutine.wrap(function() -- Perm
 			      local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
 			      if perm ~= true or adminFlr.Pads:FindFirstChild(game.Players.LocalPlayer.Name.."'s admin") ~= nil then 
