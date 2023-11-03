19d40cbd24ac6e2cc4c71346caa587864e45ea75
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-03 19:53:36 +0000

    Update Untitled Announcement.lua

--- Untitled Announcement.lua
+++ Untitled Announcement.lua
@@ -1,7 +1,7 @@
 game.Players.LocalPlayer.Chatted:Connect(function(msg)
      local command = string.lower(msg)
-      if string.sub(msg:lower(), 0, 7) == ".secret" then 
-           secret = string.sub(msg:lower(), 9)
+      if string.sub(msg:lower(), 1, 7) == ".secret" then 
+           secret = string.sub(msg, 9)
 	         game.Players:Chat("h \n\n\n "..secret.. " \n\n\n")
       end
 end)
