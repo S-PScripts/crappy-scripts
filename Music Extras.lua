219ef59e6f6bd88ceda60397ca3e086c5e8d04f4
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-03 16:51:26 +0000

    Update Music Extras.lua

--- Music Extras.lua
+++ Music Extras.lua
@@ -16,10 +16,17 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
             end
       	end
     end
-    if command == ".stopmusic" then
+    if command == ".stopmusic" then -- my executor is down and i have no fricking idea what this does. ..
 	for i, v in pairs(game:GetDescendants()) do
         	if v:IsA("Sound") then
                         v:Stop()
+        	end
+        end   
+    end
+    if command == ".startmusic" then  -- i think this and stopmusic may be the same as pause and play!!!!!!!!!!!!!!
+	for i, v in pairs(game:GetDescendants()) do
+        	if v:IsA("Sound") then
+                        v:Play()
         	end
         end   
     end
