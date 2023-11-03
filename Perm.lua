10e7a1a692aef506f1d43b2e161d1eb13ad86e3b
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-03 09:04:05 +0000

    cl eab

--- Perm.lua
+++ Perm.lua
@@ -11,24 +11,23 @@ end)
 
 task.spawn(function()
     while true do 
-        task.wait() -- It uses while true do instead now
+        task.wait()
         coroutine.wrap(function() -- PadAbuse
 			      if padAbuse == true then
-				      local pads = game:GetService("Workspace").Terrain["_Game"].Admin.Pads:GetChildren("Head")
+				local pads = game:GetService("Workspace").Terrain["_Game"].Admin.Pads:GetChildren("Head")
 			      	for i, pad in pairs(pads) do
 					        coroutine.wrap(function()
 						          pcall(function()
-							              local cre = pad.Head
+							        local cre = pad.Head
 						              	local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
 						              	firetouchinterest(cre, spr, 1)
-                            firetouchinterest(cre, spr, 0)
-                            firetouchinterest(cre, spr, 1)
-							              task.wait()
-							              firetouchinterest(cre, spr, 0)
-							
-							              if pad.Name ~= game.Players.LocalPlayer.Name.."'s admin" then
+                            					firetouchinterest(cre, spr, 0)
+                          					firetouchinterest(cre, spr, 1)
+							        task.wait()
+								firetouchinterest(cre, spr, 0)
+							        if pad.Name ~= game.Players.LocalPlayer.Name.."'s admin" then
 							                	fireclickdetector(adminFlr.Regen.ClickDetector, 0)
-							              end
+							        end
 						          end)
 					        end)()
 				      end
@@ -43,8 +42,8 @@ task.spawn(function()
 					      local pad = adminFlr.Pads:FindFirstChild("Touch to get admin")
 					      local a = pad.Head
 					      firetouchinterest(a, spr, 1)
-                firetouchinterest(a, spr, 0)
-                firetouchinterest(a, spr, 1)
+                			      firetouchinterest(a, spr, 0)
+                			      firetouchinterest(a, spr, 1)
 					      task.wait()
 					      firetouchinterest(a, spr, 0)
 				    end)
