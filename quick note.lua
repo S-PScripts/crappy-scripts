ebb87775675beefffc699408fe00be8ba217043c
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-03 16:57:59 +0000

    Create quick note.lua

---
+++ quick note.lua
@@ -0,0 +1,5 @@
+print("ok so currently NONE of my scripts have a prefix changer")
+print("it's mainly because i'm LAZY")
+print("i will eventually add one but you're need to wait :P")
+
+cmdprefix = "wip"
