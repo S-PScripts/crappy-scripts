b902d306ff1b2305b5a7ca8e239176dbdec7c47c
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-03 20:11:59 +0000

    Update Prefixing.lua

--- Prefixing.lua
+++ Prefixing.lua
@@ -1,9 +1,9 @@
-print("ok so currently NONE of my scripts have a prefix changer") print("it's mainly because i'm LAZY") print("i will eventually add one but you're need to wait :P")
+print("ok so currently NONE of my scripts have a prefix changer\nit's mainly because i'm LAZY\ni will eventually add one but you're need to wait :P")
 
-cmdprefix = "."
+cmdprefix = "." -- Make it ONE character!!
 
 game.Players.LocalPlayer.Chatted:Connect(function(msg)
-      if string.sub(msg, 1, #cmdprefix+2) == cmdprefix..'newprefix' then 
-          cmdprefix = string.sub(msg, #cmdprefix+4, #msg)	        
+  	if string.sub(msg:lower(), 0, 10) == cmdprefix.."newprefix" then
+            cmdprefix = string.sub(msg:lower(), 11, 11)
       end
 end)
