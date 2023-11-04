26f3f752fd99682b7d4d580deebcb011f7a53fbd
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-04 16:37:55 +0000

    Update My Music Only.lua

--- My Music Only.lua
+++ My Music Only.lua
@@ -1,25 +1,25 @@
-local function musiclover()
+local function musiclover()  
   while true do
     if mymusiconly == true and musicoff == false then
 		  if game:GetService("Workspace").Terrain["_Game"].Folder:FindFirstChild("Sound") then
 				if game:GetService("Workspace").Terrain["_Game"].Folder.Sound.SoundId == "http://www.roblox.com/asset/?id="..mymusiconlyid then
-                    game.Players:Chat("")
-    			else
-    				game.Players:Chat("music "..mymusiconlyid)
-                end
-          end
-          if not game:GetService("Workspace").Terrain["_Game"].Folder:FindFirstChild("Sound") then
+                    			game.Players:Chat("")
+    				else
+    					game.Players:Chat("music "..mymusiconlyid)
+				end
+               	  end
+         	 if not game:GetService("Workspace").Terrain["_Game"].Folder:FindFirstChild("Sound") then
 				game.Players:Chat("music "..mymusiconlyid)
-          end
+          	 end
+    end
     time.wait(0)
-   end
   end
 end
 
 musicoff = true
 game.Players.LocalPlayer.Chatted:Connect(function(msg)
     local command = string.lower(msg)
-    if  string.sub(msg:lower(), 0, 10) == ".permmusic" then
+    if string.sub(msg:lower(), 0, 10) == ".permmusic" then
         musicoff = false
         mymusiconly = true
         mymusiconlyid = string.sub(msg:lower(), 12)
