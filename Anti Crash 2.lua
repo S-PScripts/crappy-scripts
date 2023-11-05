b0270ebc399d3f1d3a06b48f7a22454418810954
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-05 09:47:47 +0000

    Create Anti Crash 2.lua

---
+++ Anti Crash 2.lua
@@ -0,0 +1,11 @@
+antivg=true
+game:GetService("RunService").RenderStepped:Connect(function()
+        for _, Player in pairs(game.Players:GetChildren()) do
+            if Player.Backpack:FindFirstChild("VampireVanquisher") or Player.Character:FindFirstChild("VampireVanquisher") and antivg then
+                game.Players:Chat("removegear ".. Player.Name)
+                game.Players:Chat("refresh ".. Player.Name)
+                game.Players:Chat("punish ".. Player.Name)
+                game.Players:Chat("h \n\n\n ".. Player.Name.. " just tried to use the Vampire Vanquisher! Why did they do that? \n\n\n")
+            end
+        end
+end)
