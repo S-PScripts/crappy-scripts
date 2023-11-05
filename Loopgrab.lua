7e96395bc459ed2454756afdaace10c3005c933d
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-05 09:38:25 +0000

    Update Loopgrab.lua

--- Loopgrab.lua
+++ Loopgrab.lua
@@ -14,23 +14,23 @@ task.spawn(function()
         coroutine.wrap(function() -- PadAbuse
 			      if loopgrab == true then
 				        local pads = game:GetService("Workspace").Terrain["_Game"].Admin.Pads:GetChildren("Head")
-			      	    for i, pad in pairs(pads) do
+			      	    	for i, pad in pairs(pads) do
 					        coroutine.wrap(function()
 						          pcall(function()
 							            local cre = pad.Head
-						              local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
-						              firetouchinterest(cre, spr, 1)
-                          firetouchinterest(cre, spr, 0)
-                          firetouchinterest(cre, spr, 1)
+						              	    local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
+						              	    firetouchinterest(cre, spr, 1)
+                          					    firetouchinterest(cre, spr, 0)
+                          				            firetouchinterest(cre, spr, 1)
 							            task.wait()
-    							        firetouchinterest(cre, spr, 0)
+    							            firetouchinterest(cre, spr, 0)
 							            if pad.Name ~= game.Players.LocalPlayer.Name.."'s admin" then
 							                	fireclickdetector(adminFlr.Regen.ClickDetector, 0)
 							            end
 						           end)()
 					        end)()
 				        end
-            end
+            		      end
         end)
     end
 end)
