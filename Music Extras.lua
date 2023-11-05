2585da6f53234ee429193c996f7b4bc88f839bf8
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-05 11:44:39 +0000

    Update Music Extras.lua

--- Music Extras.lua
+++ Music Extras.lua
@@ -5,15 +5,15 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
     if command == ".pause" then
        for i,v in pairs(workspace.Terrain._Game.Folder:GetDescendants()) do
 	          if v:IsA("Sound") and v.Playing then
-              	v.Playing = false
-          	end
+              	     v.Playing = false
+          	  end
        end
     end
     if command == ".play" then
       	for i,v in pairs(workspace.Terrain._Game.Folder:GetDescendants()) do
-          	if v:IsA("Sound") and not v.Playing then
-              	v.Playing = true
-            end
+          	  if v:IsA("Sound") and not v.Playing then
+              	     v.Playing = true
+                  end
       	end
     end
     if command == ".stopmusic" then -- my executor is down and i have no fricking idea what this does. ..
@@ -30,4 +30,15 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
         	end
         end   
     end
+   if string.sub(msg:lower(), 1, 7) == ".volume" then
+   	local newVolume = tonumber(string.sub(msg:lower(), 9))
+   	if newVolume ~= nil and workspace.Terrain._Game.Folder:FindFirstChild("Sound") then
+      		workspace.Terrain._Game.Folder.Sound.Volume = newVolume
+   	end
+   end
+   if command == ".musicid" then
+	if workspace.Terrain._Game.Folder:FindFirstChild("Sound") then
+			print("Current Music ID: "..workspace.Terrain._Game.Folder.Sound.SoundId)
+	end
+   end
 end)
