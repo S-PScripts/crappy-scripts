f264a559d06008ea2b42bc9c86a17b46b693e105
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-05 09:51:46 +0000

    Update Anti Crash 2.lua

--- Anti Crash 2.lua
+++ Anti Crash 2.lua
@@ -1,11 +1,16 @@
+-- updated from the one in my other repo to not stop you from crashing with vg
+-- still bad because only works with vv, not the sword and shield
+
 antivg=true
 game:GetService("RunService").RenderStepped:Connect(function()
         for _, Player in pairs(game.Players:GetChildren()) do
             if Player.Backpack:FindFirstChild("VampireVanquisher") or Player.Character:FindFirstChild("VampireVanquisher") and antivg then
+               if Player ~= game.Players.LocalPlayer then
                 game.Players:Chat("removegear ".. Player.Name)
                 game.Players:Chat("refresh ".. Player.Name)
                 game.Players:Chat("punish ".. Player.Name)
                 game.Players:Chat("h \n\n\n ".. Player.Name.. " just tried to use the Vampire Vanquisher! Why did they do that? \n\n\n")
+               end
             end
         end
 end)
