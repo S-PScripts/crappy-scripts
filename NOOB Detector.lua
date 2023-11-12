e3b8ece85317ec105c9fe74cf0e23fec41bfc30e
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-12 13:40:48 +0000

    Update NOOB Detector.lua

--- NOOB Detector.lua
+++ NOOB Detector.lua
@@ -9,15 +9,23 @@ plr.Chatted:Connect(function(msg)
                    print(v.Name..' is a noob.')
                    game.Players:Chat('h '..v.Name..', it is fly me, not ;fly!!')
                 end
+                if message:lower():find(";poop") or message:lower():find(":poop") or message:lower():find("poop") then
+                   print(v.Name..' is a noob.')
+                   game.Players:Chat('h '..v.Name..', go back to FREE ADMIN, POOP IS NOT A COMMAND!!')
+                end
+                if message:lower():find(";morph") or message:lower():find(":morph") or message:lower():find("morph") then
+                   print(v.Name..' is a noob.')
+                   game.Players:Chat('h '..v.Name..', go back to FREE ADMIN, MORPH IS NOT A COMMAND!!')
+                end
                 if message:lower() == ";fly me" then
                    print(v.Name..' is a noob.') 
                    game.Players:Chat('h '..v.Name..', imagine using the ; prefix! You dont need any!!')
                 end
-                if message:lower():find(";bring") or message:lower():find(":bring") and v ~= game.Players.LocalPlayer then
+                if message:lower():find(";bring") or message:lower():find(":bring") or message:lower():find("bring") and v ~= game.Players.LocalPlayer then
                    print(v.Name..' is a noob.')
                    game.Players:Chat('h '..v.Name..', it is tp (plr) me, not ;bring!!')
                 end
-                if message:lower():find(";goto") or message:lower():find(":goto") and v ~= game.Players.LocalPlayer then
+                if message:lower():find(";goto") or message:lower():find(":goto") or message:lower():find("goto") and v ~= game.Players.LocalPlayer then
                    print(v.Name..' is a noob.') 
                    game.Players:Chat('h '..v.Name..', it is tp me (plr), not ;goto!!')
                 end
