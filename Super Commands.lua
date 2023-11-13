9e5c731c7da200c4eb47bcf63859350d76250b41
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-13 21:00:18 +0000

    Update Super Commands.lua

--- Super Commands.lua
+++ Super Commands.lua
@@ -1,9 +1,12 @@
 game.Players.LocalPlayer.Chatted:Connect(function(msg)
      local command = string.lower(msg)
       if string.sub(msg:lower(), 1, 7) == ".superc" then 
-           for i in 1,100 do
+           for i in 1,amon do
                  game.Players:Chat(string.sub(msg:lower(), 9))
                  task.wait()
            end
       end
+     if string.sub(msg:lower(), 1, 8) == ".superch" then 
+                amon = string.sub(msg:lower(), 10)
+     end
 end)
