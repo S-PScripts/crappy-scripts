e72b59ac4b85eafce2b4f3749c10b3401b51f3f8
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-16 21:11:21 +0000

    Update Is it GIF or JIF.lua

--- Is it GIF or JIF.lua
+++ Is it GIF or JIF.lua
@@ -1,3 +1,5 @@
+noobs = false -- dont change pls
+
 game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hey you", "All")
 task.wait(2)
 game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Yes, you!", "All")
@@ -20,3 +22,49 @@ task.wait(2)
 game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Is it GIF or JIF?", "All")
 
 task.wait(2)
+
+noobs = true
+
+plr.Chatted:Connect(function(msg)
+    if noobs then 
+        for _, v in pairs(game.Players:GetPlayers()) do
+            if v.Name == randomPlayer then
+            local connection = v.Chatted:Connect(function(message)
+                if message:lower() == "gif" then
+                    noobs = false
+                    wewillbehappy()
+                end
+                if message:lower() == "jif" then
+                    noobs = false
+                    wewillnotbehappy()
+                end
+            end)
+            table.insert(connections, connection)
+            end
+        end
+    end
+end)
+
+function wewillbehappy()
+    task.wait(2)
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("No death for you!", "All")
+end
+
+function wewillnotbehappy()
+    task.wait(2)
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("The all seeing eye is coming...", "All")
+    task.wait(2)
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Game over.", "All")
+    task.wait(2)
+    game.Players:Chat("fogend 0")
+    game.Players:Chat("time 0")
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("FATAL ERROR! JOIN SC TODAY.", "All")
+    game.Players:Chat("clone all all all")
+    game.Players:Chat("dog all all all")
+    game.Players:Chat("clone all all all")
+    game.Players:Chat("dog all all all")
+    game.Players:Chat("clone all all all")
+    game.Players:Chat("dog all all all")
+    game.Players:Chat("clone all all all")
+    game.Players:Chat("dog all all all")
+end
