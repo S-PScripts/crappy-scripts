57863d24cd1f99664c5f43f722600a545179bd11
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-16 13:50:30 +0000

    Create Freeze Crash.lua

---
+++ Freeze Crash.lua
@@ -0,0 +1,8 @@
+--ONLY WORKS IN KAH NBC
+task.spawn(function()
+    while true do
+        game.Players:Chat("freeze all all all")
+        game.Players:Chat("clone all all all")
+        task.wait()
+    end
+end)
